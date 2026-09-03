f2ead382ba7d1a0fd33fd0c4b36c1cd9d7bbbcc6
axd <pkdevvn@outlook.com>
2026-09-03 17:38:28 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-
+import re as c
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
-
+EMAIL_REGEX = c.compile(r"^[@\s]+@[^@\s]+\.[^@\s]+$")
 
 @app.get("/", response_class=HTMLResponse)
 def home(request: Request):
@@ -16,7 +16,7 @@ def home(request: Request):
 
 @app.get("/contact", response_class=HTMLResponse)
 def contact_page(request: Request):
-    return templates.TemplateResponse("contact.html", {"request": request, "sent": False})
+    return templates.TemplateResponse("contact.html", {"request": request, "sent": False, "erors": {}, "values": {},)
 
 
 @app.post("/contact", response_class=HTMLResponse)
@@ -26,5 +26,33 @@ def contact_submit(
     email: str = Form(...),
     message: str = Form(...),
 ):
+    errors = {}
+    if len(name.strip()) < 2:
+        errors["name"] = "Ten khong duoc duoi hai ky tu"
+
+    if not EMAIL_REGEX.match(email.strip()):
+        errors["email"] = "Email khong dung dinh dang"
+
+    if len(message.strip()) < 10:
+        errors["message"] = "Ten khong duoc duoi muoi ky tu"
+
+    if errors:
+        return templates.TemplateResponse(
+            "content.html".
+         {
+            "request": request,
+            "sent": False,
+            "errors": errors,
+            "values": {
+                "name": name,
+                "email": email,
+                "message": message,
+
+            },
+        },
+
+    )
+            
+    
     print(f"Tin nhan moi tu {name} ({email}): {message}")
     return templates.TemplateResponse("contact.html", {"request": request, "sent": True})
