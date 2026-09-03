dd8e2c7adf83192cb614f9c1d1b8a27fe2619e36
axd <pkdevvn@outlook.com>
2026-09-03 17:39:32 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-import re as c
+import re
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
-EMAIL_REGEX = c.compile(r"^[@\s]+@[^@\s]+\.[^@\s]+$")
+EMAIL_REGEX = re.compile(r"^[@\s]+@[^@\s]+\.[^@\s]+$")
 
 @app.get("/", response_class=HTMLResponse)
 def home(request: Request):
