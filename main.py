067270f0650ed1c2097018d214a1c3b319108713
axd <pkdevvn@outlook.com>
2026-09-03 16:24:22 +0700

    dit me ban phim

--- main.py
+++ main.py
@@ -8,23 +8,23 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
+
 @app.get("/", response_class=HTMLResponse)
 def home(request: Request):
-  return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse("index.html", {"request": request})
 
-@app.get("/comtact", response_class=HTMLResponse)
+
+@app.get("/contact", response_class=HTMLResponse)
 def contact_page(request: Request):
-  return templates.TemplateResponse("contact.html", {request, "sent": False})
+    return templates.TemplateResponse("contact.html", {"request": request, "sent": False})
+
 
 @app.post("/contact", response_class=HTMLResponse)
 def contact_submit(
-  request: Request,
-  name: str = Form(...),
-  email: str = Form(...),
-  message: str = Form(...),
+    request: Request,
+    name: str = Form(...),
+    email: str = Form(...),
+    message: str = Form(...),
 ):
-  print(f"Tin nhan moi tu {name} ({email}): {message}")
-  return templates.TemplateResponse("contact.html", {request, "sent": True})
-
-# project nho :) 
-# 15:58 gmt +7 3/9/2026 
+    print(f"Tin nhan moi tu {name} ({email}): {message}")
+    return templates.TemplateResponse("contact.html", {"request": request, "sent": True})
